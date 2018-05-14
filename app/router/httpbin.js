92b6b3795639d839ae5651438758abd1ce102b50
isayme <isaymeorg@gmail.com>
2018-05-14 20:19:47 +0800

    add /user-agent

--- app/router/httpbin.js
+++ app/router/httpbin.js
@@ -4,15 +4,23 @@ const uuidv4 = require('uuid/v4')
 const express = require('express')
 const router = express.Router()
 
+const constants = require('app/constants')
+
 router.get('/ip', function (req, res) {
   res.json({
-    origin: requestIP.getClientIp(req) || 'unknown'
+    origin: requestIP.getClientIp(req) || constants.UnKnown
   })
 })
 
 router.get('/uuid', function (req, res) {
   res.json({
     uuid: uuidv4()
+  })
+})
+
+router.get('/user-agent', function (req, res) {
+  res.json({
+    'user-agent': req.header('user-agent') || constants.UnKnown
   })
 })
 
