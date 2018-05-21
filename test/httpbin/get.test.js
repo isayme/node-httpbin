c8463f7ae7a295d45875a76ed711ff2eb0f2d39b
isayme <isaymeorg@gmail.com>
2018-05-21 20:58:55 +0800

    test: get test

--- test/httpbin/get.test.js
+++ test/httpbin/get.test.js
@@ -4,7 +4,10 @@ const app = require('app/app')
 
 describe('/get', () => {
   it('default', async function () {
-    let res = await request(app).get('/get')
+    let res = await request(app)
+      .get('/get')
+      .query({ 'k': 'v' })
     assert.equal(res.statusCode, 200)
+    assert.equal(res.body.args.k, 'v')
   })
 })
