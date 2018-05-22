b0328cb6eb1da76165b8e57082743a0161449709
isayme <isaymeorg@gmail.com>
2018-05-22 18:34:22 +0800

    add test

---
+++ test/httpbin/anything.test.js
@@ -0,0 +1,21 @@
+const request = require('supertest')
+const assert = require('power-assert')
+const app = require('app/app')
+
+describe('/anything', () => {
+  it('get', async function () {
+    let res = await request(app)
+      .get('/anything')
+      .query({ 'k': 'v' })
+    assert.equal(res.statusCode, 200)
+    assert.equal(res.body.args.k, 'v')
+  })
+
+  it('post', async function () {
+    let res = await request(app)
+      .post('/anything')
+      .send({ 'k': 'v' })
+    assert.equal(res.statusCode, 200)
+    assert.equal(res.body.json.k, 'v')
+  })
+})
